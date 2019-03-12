cf6a666fff5ce9f43da629d36dd4ad9b0aff988e
Yash Baheti <zilllionaire@gmail.com>
2019-03-12 17:46:01 +0530

    moved reddit util interfaces to separate file

---
+++ src/utils/@types-reddit.ts
@@ -0,0 +1,47 @@
+export interface Gildings {
+    gid_1: number;
+    gid_2: number;
+    gid_3: number;
+}
+export const enum SubredditSorts {
+    HOT = 'hot',
+    NEW = 'new',
+    CONTROVERSIAL = 'controversial',
+    TOP = 'top',
+    RISING = 'rising',
+}
+export interface SubredditData {
+    subreddit: string;
+    type?: SubredditSorts;
+    after?: boolean | string;
+}
+
+export interface ParsedDate {
+    hour: number;
+    time: number;
+}
+export type ParseDate = (date: number) => ParsedDate;
+
+export interface ParsedGildings {
+    silver: number;
+    gold: number;
+    platinum: number;
+}
+export type ParseGildings = (Gilding: Gildings) => ParsedGildings;
+
+export interface PostTransform {
+    key: string;
+    transform: boolean | ParseDate | ParseGildings;
+}
+export interface ParsedPost {
+    created_utc: ParsedDate;
+    gildings: ParsedGildings;
+    num_comments: number;
+    num_crossposts: number;
+    score: number;
+}
+export interface FetchSubredditResponse {
+    nodes: ParsedPost[];
+    after: boolean | string;
+}
+export type FetchSubreddit = boolean | FetchSubredditResponse;
