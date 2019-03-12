d421556109aea26870697bd0df85fc0c318f94be
Yash Baheti <zilllionaire@gmail.com>
2019-03-12 17:50:15 +0530

    added start/stop logic, oldest post time

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     section.ph2
       .pa2.f5.br2.ba.mv2.b--silver.moon-gray Type in a subreddit name and press start
       form(@submit.prevent='startFetch')
+        .f6.pb1.silver Subreddit name
         .flex
           .f4.br2.br--left.bg-moon-gray.silver.pa3.ph2 /r/
           input.w-100.f4.pv3.pl2.outline-0.bn.bg-moon-gray(type='text' placeholder='r/' v-model='subreddit' required)
@@ -16,17 +17,23 @@
             option(value='top') /top
             option(value='controversial') /controversial
             option(value='rising') /rising  
-          button.f5.pa3.outline-0.bn.ttu.pointer.bg-reddit-color.white.o-80.glow(type='submit' :disabled='started') Start
-          button.f5.pa3.outline-0.bn.br2.ttu.br--right.pointer.bg-mid-gray.white.o-80.glow(@click='clearData' type='button') Clear
-        .flex.pv2
-          span.w3.ml2.pa3.ba.br--left.b--silver.moon-gray.br2 Limit
-          input.w4.f4.pa3.outline-0.br2.br--right.bn.bg-moon-gray(type='number' placeholder='Max posts' v-model='maxPosts' required)
-          span.ml2.w-100.pa3.ba.b--silver.moon-gray.br2
-            | Total posts: 
-            span.f4 {{ totalCount }}
-      .pa2.flex.flex-column.flex-row-ns
+          button.f5.pa3.outline-0.bn.ttu.pointer.white.o-80.glow(type='submit' :disabled='started' :class='buttonStyleSubmit') Start
+          button.f5.pa3.outline-0.bn.br2.ttu.br--right.pointer.white.o-80.glow(@click='buttonAction' type='button' :class='buttonStyleClear') {{ buttonLabel }}
+        .f6.pb1.silver.mt2.pb1 Limit of posts
+        .flex.pb2
+          input.w4.f4.pa3.outline-0.br2.bn.bg-moon-gray(type='number' placeholder='Max posts' v-model='maxPosts' required)
+          .ml2.w-100.pa3.ba.b--silver.moon-gray.br2.flex.justify-start
+            div
+              span.f6.pb1.silver.ma2 Total posts
+              span.bg-silver.black-70.br1.f5.pv1.ph2  {{ totalCount }}
+            div
+              span.f6.pb1.silver.ma2 Oldest post
+              span.bg-silver.black-70.br1.f5.pv1.ph2 {{ oldestPost }}
+      .ph2.flex.flex-column.flex-row-ns
         chart-score-counts
         chart-post-count
+      .ph2.flex.flex-column.flex-row-ns
+        chart-comment-counts
         chart-gild-counts
 </template>
 <script lang="ts">
@@ -34,9 +41,12 @@
   import ChartPostCount from '@/components/charts/PostCount.vue';
   import ChartScoreCounts from '@/components/charts/ScoreCounts.vue';
   import ChartGildCounts from '@/components/charts/GildCounts.vue';
-  import { fetchSubReddit, SubredditSorts, streamPosts } from '@/utils/reddit.ts';
+  import ChartCommentCounts from '@/components/charts/CommentCounts.vue';
+  import { fetchSubReddit, streamPosts } from '@/utils/reddit.ts';
   import { Actions, Getters } from '@/store/reddit/@types';
-  import { mapGetters, mapActions, mapState } from 'vuex';
+  import { mapGetters, mapActions, mapState, Computed } from 'vuex';
+  import { SubredditSorts } from '@/utils/@types-reddit';
+  import { ComputedClass } from '*.vue';
   export default Vue.extend({
     name: 'app',
     data() {
@@ -48,13 +58,47 @@
     },
     computed: {
       ...mapState('reddit', ['started']),
-      ...mapGetters('reddit', [Getters.TOTAL_COUNT]),
+      ...mapGetters('reddit', [Getters.TOTAL_COUNT, Getters.OLDEST_POST]),
+      buttonStyleSubmit(): ComputedClass {
+        return {
+          // @ts-ignore
+          'bg-gray moon-gray': this.started,
+          // @ts-ignore
+          'bg-reddit-color white': !this.started,
+        };
+      },
+      buttonStyleClear(): ComputedClass {
+        return {
+          // @ts-ignore
+          'bg-gray moon-gray': !this.started,
+          // @ts-ignore
+          'bg-reddit-color white': this.started,
+        };
+      },
+      buttonLabel(): string {
+        // @ts-ignore
+        return this.started ? 'Stop' : 'Clear';
+      },
     },
     methods: {
       ...mapActions('reddit', ['clearData', 'setStatus']),
+      buttonAction() {
+        // @ts-ignore
+        const started: boolean = this.started;
+        if (started) {
+          // @ts-ignore
+          this.setStatus({ status: false });
+        } else {
+          // @ts-ignore
+          this.clearData();
+        }
+      },
       async startFetch() {
         const name = this.subreddit;
-        const fetchConfig = { subreddit: name, type: this.sortType as SubredditSorts };
+        const fetchConfig = {
+          subreddit: name,
+          type: this.sortType as SubredditSorts,
+        };
         // @ts-ignore
         this.setStatus({ status: true });
         await streamPosts(fetchConfig, this.maxPosts);
@@ -64,6 +108,7 @@
       ChartGildCounts,
       ChartPostCount,
       ChartScoreCounts,
+      ChartCommentCounts,
     },
   });
 </script>
