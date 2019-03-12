382b447f77e7eb2fb38c17b62d6ecb09321cbbee
Yash Baheti <zilllionaire@gmail.com>
2019-03-12 17:46:52 +0530

    added common utility functions

--- src/utils/charts.ts
+++ src/utils/charts.ts
@@ -16,7 +16,6 @@ export const chartOptions = {
         ],
     },
 };
-
 export const findCount = (numbers: number[] | undefined): number =>
     numbers === undefined ? 0 : numbers.length;
 export const findTotal = (numbers: number[] | undefined): number => {
@@ -31,7 +30,9 @@ export const findMedian = (numbers: number[] | undefined): number => {
     }
     numbers.sort((a, b) => a - b);
     const length = numbers.length;
-    return length % 2 ? numbers[(length - 1) / 2] : (numbers[length / 2 - 1] + numbers[length / 2]) / 2;
+    return length % 2
+        ? numbers[(length - 1) / 2]
+        : (numbers[length / 2 - 1] + numbers[length / 2]) / 2;
 };
 export const findRange = (numbers: number[] | undefined): number => {
     if (numbers === undefined) {
@@ -41,3 +42,38 @@ export const findRange = (numbers: number[] | undefined): number => {
     const max = Math.max(...numbers);
     return max - min;
 };
+const rgba = (nums: number[], solid: boolean = true) => {
+    return `rgba(${nums[0]}, ${nums[1]}, ${nums[2]}, ${solid ? 1 : 0.2})`;
+};
+export const chartColors = (gildings: boolean = false) => {
+    const colors = [
+        [204, 0, 153],
+        [51, 102, 204],
+        [0, 255, 153],
+        [245, 124, 0],
+    ];
+    const colorsGild = [[155, 155, 155], [247, 228, 56], [0, 188, 248]];
+    return (gildings ? colorsGild : colors).map((color) => ({
+        background: rgba(color, false),
+        border: rgba(color),
+    }));
+};
+export const chartDataset = (labels: string[], gildings: boolean = false) => {
+    const colors = chartColors(gildings);
+    const datasets = labels.map((label, index) => ({
+        label,
+        data: [],
+        backgroundColor: colors[index].background,
+        borderColor: colors[index].border,
+        borderWidth: 1,
+    }));
+    return {
+        labels: chartLabels(),
+        datasets,
+    };
+};
+export const chartConfig = (labels: string[], gildings: boolean = false) => ({
+    type: 'line',
+    data: chartDataset(labels, gildings),
+    options: chartOptions,
+});
